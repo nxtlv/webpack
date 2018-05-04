9460a8e6235de017e3fb7ee42bae68e7b8dedb94
NC <nc@develup.pro>
2018-05-04 17:46:07 +0300

    fix template

--- meta.js
+++ meta.js
@@ -9,17 +9,6 @@ module.exports = {
     }
   },
   "prompts": {
-    "title": {
-      "type": "string",
-      "required": true,
-      "message": "Project name"
-    },
-    "description": {
-      "type": "string",
-      "required": false,
-      "message": "Project description",
-      "default": "A Vue.js project"
-    },
     "router": {
       "type": "confirm",
       "message": "Install vue-router?"
@@ -27,16 +16,6 @@ module.exports = {
     "vuex": {
       "type": "confirm",
       "message": "Install vuex?"
-    },
-    "proxy": {
-      "type": "confirm",
-      "message": "Do you need proxy api requests?"
-    },
-    "remoteServer": {
-        "when": "proxy",
-        "type": "string",
-        "required": true,
-        "message": "Remote server address"
     }
   },
   "filters": {
